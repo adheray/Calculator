7cf3694f0aee54c4a207f3bae2e8c1380246aee8
adairay <adairay@ALEXANDREDA583C>
2024-04-15 23:40:45 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Calculus.cs
@@ -0,0 +1,4 @@
+﻿public class Calculus
+{
+    public int Id { get; set; }
+}
---
+++ ConsoleApp1/Model.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class CalcDbContext : DbContext
+{
+    public DbSet<Calculus> Calculus { get; set; }
+
+    public string DbPath { get; }
+
+    public CalcDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "calc.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
